93f986a862bb2cc0576aa888c0d5e883f0560eaf
PAULO HENRIQUE PEREIRA DE ARAUJO SILVA <pharaujo14@gmail.com>
2025-03-19 15:32:22 -0300

    Ajuste no auxiliar.py

--- auxiliar.py
+++ auxiliar.py
@@ -1,155 +1,90 @@
+import psutil
+import smtplib
 import streamlit as st
 import requests
-import pandas as pd
-import matplotlib.pyplot as plt
 
-from datetime import datetime, timedelta
+from email.mime.text import MIMEText
 from google.oauth2.service_account import Credentials
-from google.cloud import storage
-
-import io
-import os
-import ssl
-import certifi
-import tempfile
 from google.auth.transport.requests import Request
 
-# Ignorar verificação SSL globalmente
-ssl._create_default_https_context = ssl._create_unverified_context
-
-# Forçar uso do certifi corretamente
-os.environ["SSL_CERT_FILE"] = certifi.where()
-os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
-
-# Criar uma sessão de requisição sem verificação de SSL
-session = requests.Session()
-session.verify = False
-
-# Criar credenciais a partir do Streamlit secrets
-credentials = Credentials.from_service_account_info(
-    st.secrets["google_cloud"],
-    scopes=["https://www.googleapis.com/auth/devstorage.read_write"]
-)
-
-# Atualizar token manualmente sem session
-request = Request()
-if not credentials.valid:
-    credentials.refresh(request)
-
-# Criar cliente de armazenamento
-client = storage.Client(credentials=credentials)
-
-# Configuração do Google Cloud Storage
-GCS_BUCKET_NAME = "cookies-c4"
-
-API_UPLOAD_URL = f"https://storage.googleapis.com/upload/storage/v1/b/{GCS_BUCKET_NAME}/o?uploadType=resumable"
-
-# Carregar credenciais da API do arquivo secrets.toml
-ORGANIZATIONS = {
-    "property": {
-        "x-api-key": st.secrets["api"]["property_api_key"],
-        "x-api-secret": st.secrets["api"]["property_api_secret"],
-        "x-org-id": st.secrets["api"]["property_org_id"]
-    },
-    "carrefour": {
-        "x-api-key": st.secrets["api"]["carrefour_api_key"],
-        "x-api-secret": st.secrets["api"]["carrefour_api_secret"],
-        "x-org-id": st.secrets["api"]["carrefour_org_id"]
-    },
-    "sams": {
-        "x-api-key": st.secrets["api"]["sams_api_key"],
-        "x-api-secret": st.secrets["api"]["sams_api_secret"],
-        "x-org-id": st.secrets["api"]["sams_org_id"]
-    },
-    "cci": {
-        "x-api-key": st.secrets["api"]["cci_api_key"],
-        "x-api-secret": st.secrets["api"]["cci_api_secret"],
-        "x-org-id": st.secrets["api"]["cci_org_id"]
-    }
-}
-
-API_URL = "https://app.securiti.ai/reporting/v1/sources/query?ref=getCmpCookieConsentRecords"
-
-def fetch_cookie_data(start_date, end_date):
-    """Baixa dados de cookies para um intervalo de tempo específico."""
-    all_data = []
-    start_ts = int(start_date.timestamp())
-    end_ts = int(end_date.timestamp())
+# Função para envio de email
+def enviar_resultado(subject, body, sender, recipients, password):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
     
-    for org_name, credentials in ORGANIZATIONS.items():
-        offset = 0
-        while True:
-            headers = {
-                "x-tident": st.secrets["api"]["x_tident"],
-                "x-api-key": credentials["x-api-key"],
-                "x-api-secret": credentials["x-api-secret"],
-                "x-org-id": credentials["x-org-id"]
-            }
-            payload = {
-                "source": "category_consents_flat",
-                "response_config": {"format": 1},
-                "pagination": {"type": "limit-offset", "offset": offset, "limit": 100000, "omit_total": True},
-                "fields": [],
-                "order_by": ["-activity_timestamp"],
-                "skip_cache": True,
-                "filter": {
-                    "op": "and",
-                    "value": [
-                        {"op": "gte", "field": "activity_timestamp", "value": start_ts},
-                        {"op": "lte", "field": "activity_timestamp", "value": end_ts}
-                    ]
-                }
-            }
-            
-            response = session.post(API_URL, json=payload, headers=headers)
-            if response.status_code == 200:
-                data = response.json()
-                records = data.get("data", [])
-                if not records:
-                    break
-                for record in records:
-                    record["organization"] = org_name
-                all_data.extend(records)
-                offset += 100000
-            else:
-                st.error(f"Erro ao buscar dados para {org_name}: {response.status_code}")
-                break
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+        smtp_server.login(sender, password)
+        smtp_server.sendmail(sender, recipients, msg.as_string())
     
-    return pd.DataFrame(all_data)
+    print("Email enviado com sucesso!")
 
-def upload_parquet_gcs(df, filename):
-    """Faz upload de um DataFrame como Parquet no Google Cloud Storage (GCS)."""
+# Função para verificar se o arquivo está em uso
+def is_file_in_use(file_path):
+    for proc in psutil.process_iter(['open_files']):
+        try:
+            for file in proc.info['open_files'] or []:
+                if file.path == file_path:
+                    return True
+        except (psutil.AccessDenied, psutil.NoSuchProcess):
+            continue
+    return False
+
+# Configuração da Conta de Serviço para o Google Drive
+def upload_to_drive(file_name, file_path, folder_id, custom_name):
     try:
-        output = io.BytesIO()
-        df.to_parquet(output, engine="pyarrow")
-        output.seek(0)
-        blob = client.bucket(GCS_BUCKET_NAME).blob(filename)
-        blob.upload_from_file(output, content_type="application/octet-stream")
-        st.write(f"📤 Arquivo {filename} enviado para GCS!")
-    except Exception as e:
-        st.error(f"Erro ao fazer upload para o Google Cloud Storage: {e}")
+        # Carregar as credenciais do `secrets.toml`
+        credentials = Credentials.from_service_account_info(
+            st.secrets["google_drive"],
+            scopes=["https://www.googleapis.com/auth/drive"]
+        )
 
-def preencher_dias_faltantes():
-    """Verifica se há arquivos ausentes no Google Cloud Storage e baixa os dados necessários."""
-    data_atual = datetime.now()
-    data_inicio = datetime(2024, 3, 1)
-    dias_faltantes = []
+        # Atualizar o token manualmente
+        session = requests.Session()
+        session.verify = False  # Ignorar verificação de certificado SSL
+        request = Request(session=session)
+        if not credentials.valid:
+            credentials.refresh(request)
 
-    existing_blobs = {blob.name for blob in client.bucket(GCS_BUCKET_NAME).list_blobs(timeout=300)}
+        # URL do Google Drive para upload
+        url = "https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable"
+        headers = {
+            "Authorization": f"Bearer {credentials.token}",
+            "Content-Type": "application/json"
+        }
 
-    while data_inicio < data_atual:
-        filename = f"cookies_{data_inicio.strftime('%Y-%m-%d')}.parquet"
-        if filename not in existing_blobs:
-            dias_faltantes.append(data_inicio)
-        data_inicio += timedelta(days=1)
+        # Configuração dos metadados do arquivo
+        metadata = {
+            "name": custom_name,
+            "parents": [folder_id]
+        }
+        response = session.post(url, headers=headers, json=metadata)
+        print(f"Response Status: {response.status_code}")
+        print(f"Response Content: {response.content.decode()}")
 
-    for dia in dias_faltantes:
-        df = fetch_cookie_data(dia, dia + timedelta(days=1))
-        if not df.empty:
-            upload_parquet_gcs(df, f"cookies_{dia.strftime('%Y-%m-%d')}.parquet")
+        if response.status_code not in [200, 201]:
+            st.error(f"Erro ao inicializar o upload: {response.content.decode()}")
+            return None
+
+        # URL de upload retornada pelo Google
+        upload_url = response.headers["Location"]
+        if not upload_url:
+            print("Erro: URL de upload não encontrada no cabeçalho.")
+            return
+
+        # Upload do arquivo
+        with open(file_path, "rb") as f:
+            upload_response = session.put(upload_url, headers={"Content-Type": "application/octet-stream"}, data=f)
+            print(f"Upload Response Status: {upload_response.status_code}")
+            print(f"Upload Response Content: {upload_response.content.decode()}")
+
+        if upload_response.status_code in [200, 201]:
+            file_id = upload_response.json().get("id")
+            return file_id
         else:
-            st.write(f"⚠️ Nenhum dado encontrado para {dia.strftime('%Y-%m-%d')}")
-
-# Executar a verificação automática após meia-noite
-if datetime.now().hour >= 0:
-    preencher_dias_faltantes()
+            st.error(f"Erro ao fazer upload do arquivo: {upload_response.content.decode()}")
+            return None
+    except Exception as e:
+        st.error(f"Erro ao fazer upload para o Google Drive: {e}")
+        return None
